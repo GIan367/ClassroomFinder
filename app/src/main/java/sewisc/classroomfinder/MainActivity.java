5f40fe17dfc23bfff3c8c2f261ddb614201b6175
Zak <zkocken@wisc.edu>
2017-03-14 21:45:56 -0500

    Replaced the ugly SearchView items with slick and semi-functional SearchableSpinners from a library I found.

--- app/src/main/java/sewisc/classroomfinder/MainActivity.java
+++ app/src/main/java/sewisc/classroomfinder/MainActivity.java
@@ -1,14 +1,8 @@
 package sewisc.classroomfinder;
 
-import android.app.Activity;
-import android.app.SearchManager;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ListAdapter;
 import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
@@ -52,27 +46,5 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    // Got rid of settings action bar item because we don't need it.
-    // Also commented out relevant stuff in menu_main.xml -- Zak
-    /*@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
-
-    public void openSearch(View view) {
-        Intent intent = new Intent(this, LocationSearch.class);
-        startActivity(intent);
     }
 }
