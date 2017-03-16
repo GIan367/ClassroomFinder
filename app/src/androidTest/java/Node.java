e1b641a74bdac6ec96a929e892e4bd1fc5cb551c
shreyashstha <shreyashstha@gmail.com>
2017-03-15 20:54:34 -0500

    Modified Node class to include floor variable
    Adds floor variable to the Node class along with a getter. The Node
    constructor is modified to include floor as well.

--- app/src/androidTest/java/Node.java
+++ app/src/androidTest/java/Node.java
@@ -6,6 +6,7 @@ public class Node {
     //Private Variables
     private NodeType type;              //Type of Node
     private int relativeX, relativeY;	//Relative x and y coordinates on the map
+    private int floor;                  //The floor this node is located in
     private String name;                //Name of this Node object
 
     //Getters for type, relativeX, relativeY, and name
@@ -21,15 +22,20 @@ public class Node {
         return relativeY;
     }
 
+    public int getFloor() {
+        return floor;
+    }
+
     public String getName() {
         return name;
     }
 
     //Constructor
-    public Node (NodeType type, int relX, int relY, String name){
+    public Node (NodeType type, int relX, int relY, int floor, String name){
         this.type = type;
         this.relativeX = relX;
         this.relativeY = relY;
+        this.floor = floor;
         this.name = name;
     }
 }
