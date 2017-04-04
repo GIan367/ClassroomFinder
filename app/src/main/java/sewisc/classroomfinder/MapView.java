314f2b9e03d076b125ceb4d29ff9854cebb84c64
mwong678 <matthew.wong@wisc.edu>
2017-04-04 13:45:28 -0500

    Added drawing

--- app/src/main/java/sewisc/classroomfinder/MapView.java
+++ app/src/main/java/sewisc/classroomfinder/MapView.java
@@ -17,6 +17,12 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -47,14 +53,76 @@ public class MapView extends AppCompatActivity {
         toast = Toast.makeText(getApplicationContext(), "Path saved to favorites.", Toast.LENGTH_SHORT);
         dataBaseHandler = new DataBaseHandler(this);
 
+        List<Node> nodes = new ArrayList<Node>();
+        InputStream stream = null;
+        XMLParser xmlParser = new XMLParser();
+        List<XMLParser.Entry> entries = null;
+        try {
+            //new FileInputStream(new File("easttowne.xml"));
+            stream = getAssets().open("easttowne.xml");
+            entries = xmlParser.parse(stream);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("xml file not found");
+        } catch (XmlPullParserException e) {
+            System.out.println("parser not working");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (stream != null) {
+            try {
+                stream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        List<Node> testNodes = new ArrayList<Node>();
+        testNodes.add(new Node(NodeType.normal, 4710, 3735, 1, "Amplified Phones"));
+        testNodes.add(new Node(NodeType.normal, 5215, 4905, 1, "Dick's"));
+        testNodes.add(new Node(NodeType.normal, 1905, 3975, 1, "Boston Store"));
+        testNodes.add(new Node(NodeType.normal, 995, 3165, 1, "Sears"));
+        testNodes.add(new Node(NodeType.normal, 5847, 3160, 1, "JCPenney"));
+        testNodes.add(new Node(NodeType.normal, 4710, 3735, 1, "Amplified Phones 2"));
+        //uses parser to add all the nodes to a list; list should be used to construct building
+        //class which will then be used to make the graph for the A* which will then be used to
+        //choose a list of nodes for the path used for drawPath (I believe)
+        for(XMLParser.Entry entry: entries) {
+            Node node = null;
+            //I have to use a switch case here because I cannot use one instantiation statement
+            //to account for all the enum values required for the first parameter.
+            switch (entry.type){
+                case ("Normal"):
+                    node = new Node(NodeType.normal, entry.x, entry.y, entry.z, entry.name);
+                    break;
+                case ("Bathroom"):
+                    node = new Node(NodeType.bathroom, entry.x, entry.y, entry.z, entry.name);
+                    break;
+                case ("Stair"):
+                    node = new Node(NodeType.stair, entry.x, entry.y, entry.z, entry.name);
+                    break;
+                case ("Elevator"):
+                    node = new Node(NodeType.elevator, entry.x, entry.y, entry.z, entry.name);
+                    break;
+            }
+            if (node != null){
+                nodes.add(node);
+            }
+            //System.out.println("Name: " + entry.name + " Type " + entry.type + " X: "
+           //         + entry.x + " Y: " + entry.y + " Z: " + entry.z);
+        }
+
+
+
         if(dest != null) { // All text fields populated -- standard Room Finder AStar
             //TODO: call AStar, determine correct map image to draw on (currently dummy value)
             int id = getResources().getIdentifier("east_towne1", "mipmap", getPackageName());
-            drawLine(id, null);
+            drawPath(id, testNodes);
         } else if(loc != null) { // No destination populated -- Bathroom Finder AStar
             //TODO: call AStar, determine correct map image to draw on (currently dummy value)
             int id = getResources().getIdentifier("east_towne1", "mipmap", getPackageName());
-            drawLine(id, null);
+            drawPath(id, testNodes);
         } else { // No text fields populated -- only displaying a floor
             imageView.setImageResource(ref);
         }
@@ -74,10 +142,11 @@ public class MapView extends AppCompatActivity {
 
     // Draw line on given map image reference from current location to destination via list of nodes
     // TODO: Call AStar to get nodes and pass here -- currently hardcoded to draw a simple line
-    void drawLine(int ref, List<Node> nodes){
+    void drawPath(int ref, List<Node> nodes){
         BitmapFactory.Options myOptions = new BitmapFactory.Options();
         myOptions.inDither = true;
         myOptions.inScaled = false;
+
         myOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;// important
         myOptions.inPurgeable = true;
 
@@ -86,17 +155,29 @@ public class MapView extends AppCompatActivity {
         Paint paint = new Paint();
         paint.setAntiAlias(true);
         paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(25);
+        paint.setFilterBitmap(true);
+        paint.setStrokeWidth(15);
 
         Bitmap workingBitmap = Bitmap.createBitmap(bitmap);
         Bitmap mutableBitmap = workingBitmap.copy(Bitmap.Config.ARGB_8888, true);
 
-
+        double scale = (2100.0/6998.0);
+        System.out.println(scale);
         Canvas canvas = new Canvas(mutableBitmap);
         //canvas.drawCircle(60, 50, 25, paint);
 
         // TODO: loop through list of nodes from AStar; currently draws a simple test line
-        canvas.drawLine(0, 0, 691, 492, paint);
+       // canvas.drawLine(0, 0, 2000, 2000, paint);
+        Node prevNode = null;
+        for (Node n: nodes){
+
+            if (prevNode != null){
+                //first case
+                canvas.drawLine((int) (prevNode.getRelativeX() * scale), (int) (prevNode.getRelativeY() * scale), (int) (n.getRelativeX() * scale), (int) (n.getRelativeY() * scale), paint);
+                //System.out.println((int) (prevNode.getRelativeX() * scale));
+            }
+            prevNode = n;
+        }
 
         imageView.setAdjustViewBounds(true);
         imageView.setImageBitmap(mutableBitmap);
