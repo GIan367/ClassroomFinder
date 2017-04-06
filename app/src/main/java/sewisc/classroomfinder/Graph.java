42d35dcf2e2e0808d4440247db4b8b13f49c976c
shreyashstha <shreyashstha@gmail.com>
2017-04-06 17:27:26 -0500

    Update Graph.java
    Changed cost function to check destination type.

--- app/src/main/java/sewisc/classroomfinder/Graph.java
+++ app/src/main/java/sewisc/classroomfinder/Graph.java
@@ -14,9 +14,9 @@ public class Graph {
     //Function that returns the cost of movement from one node to another
     public int cost (Node A, Node B){
         //TODO: We will need to change cost function to reflect actual cost. This will be based on dimension of the map.
-        if (A.getType() == NodeType.normal) return 10;
-        if (A.getType() == NodeType.stair) return 15;
-        if (A.getType() == NodeType.elevator) return 11;
+        if (B.getType() == NodeType.normal) return 10;
+        if (B.getType() == NodeType.stair) return 15;
+        if (B.getType() == NodeType.elevator) return 11;
         return 10;
     }
 
